af8d95776a0db665828423f531cefd4214784e4d
Hugo Dupraz <hugo.dupraz@arcanite.ch>
2020-02-01 00:57:46 +0100

    there are enzym and they move

--- godot/Objet.gd
+++ godot/Objet.gd
@@ -3,6 +3,8 @@ class_name Objet
 
 
 export var hp = 100.0
+export var enzym = false
+export var degradation_rate = 0.0
 
 func damage(n):
     hp -= n
@@ -10,4 +12,7 @@ func damage(n):
     set_modulate(Color(1, ratio, ratio, 1)) 
 
 func is_enzym():
-    return false
+    return enzym
+
+func _physics_process(delta):
+    hp -= max(0.0, delta * degradation_rate)
