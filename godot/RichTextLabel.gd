495d525589ccff7da96323a78c998c6f90c996c2
Hugo Dupraz <hugo.dupraz@arcanite.ch>
2020-02-01 23:56:27 +0100

    change text display

---
+++ godot/RichTextLabel.gd
@@ -0,0 +1,31 @@
+extends RichTextLabel
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var tt = 0.0
+var t = 0.0
+var cps = 115.0
+var c = 0
+onready var base_text = get_node("../text").bbcode_text
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+func _physics_process(delta):
+    tt += delta
+    t += delta
+    if t > 1 / cps:
+        t -= 1 / cps
+        if base_text[c] == "#":
+            print(tt)
+            c += 1
+        else:
+            bbcode_text += base_text[c]
+            if base_text[c] == "\n":
+                t -= 1.0
+            c += 1
+        
